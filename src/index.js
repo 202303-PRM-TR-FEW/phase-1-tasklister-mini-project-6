98145038355b3346aeb50d70043ae337d734d736
haroon <haroon.seena2017@gmail.com>
2023-06-21 23:26:28 +0300

    i will work on it

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
+const input = document.getElementById("new-task-description");
+const btn = document.getElementById("btn");
+const ul = document.getElementById("tasks");
+const li = document.createElement("li");
+const btn2 = document.createElement("span");
+btn2.style.padding = "5px";
+btn2.style.cursor = "pointer";
+li.classList.add("remove-btn");
 
-  const input = document.getElementById("new-task-description");
-  const btn = document.getElementById("btn");
-  const ul = document.getElementById("tasks");
-  const li = document.createElement("li");
-  const btn2 = document.createElement("span");
-  btn2.style.padding = "5px";
-  btn2.style.cursor = "pointer";
-  li.classList.add("remove-btn");
-
-  btn.addEventListener("click", function (e) {
-    e.preventDefault();
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
     li.innerHTML = input.value;
     ul.append(li);
     input.value = "";
     btn2.innerHTML = "delete";
     li.append(btn2);
-  });
- 
+  
+});
+btn2.addEventListener("click",function(){
+  li.remove();
+});
