7a192005e3c2c14d7d75d3252baefbde56362eb4
haroon <haroon.seena2017@gmail.com>
2023-06-21 22:54:22 +0300

    i will continue

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+  const input = document.getElementById("new-task-description");
+  const btn = document.getElementById("btn");
+  const ul = document.getElementById("tasks");
+  const li = document.createElement("li");
+  const btn2 = document.createElement("span");
+  btn2.style.padding = "5px";
+  btn2.style.cursor = "pointer";
+  li.classList.add("remove-btn");
+
+  btn.addEventListener("click", function (e) {
+    e.preventDefault();
+    li.innerHTML = input.value;
+    ul.append(li);
+    input.value = "";
+    btn2.innerHTML = "delete";
+    li.append(btn2);
+  });
+ 
